74076445ff1874d61ad3262bd5a96885c86d7d9b
RuchirVyas <ruchirvyasb@gmail.com>
2026-06-07 11:52:52 +0530

    frontend changes

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from schemas import UserCreate, UserLogin
 
 app = FastAPI(
     title="Cloud Bus Pass System API"
+)
+
+# CORS Configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
@@ -23,5 +36,6 @@ def register(user: UserCreate):
 def login(user: UserLogin):
     return {
         "message": "Login successful",
-        "email": user.email
+        "email": user.email,
+        "access_token": "dummy_token_123"
     }
