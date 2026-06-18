66831d8829cc13829d932d4a1341b397833fc000
unknown <ruchirvyasb@gmail.com>
2026-06-18 17:54:09 +0530

    temporary cors fix

--- backend/main.py
+++ backend/main.py
@@ -45,13 +45,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://stupendous-pothos-cc3a53.netlify.app",
-        "http://localhost:3000",
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
